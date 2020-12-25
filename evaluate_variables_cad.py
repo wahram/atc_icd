b08adc76c1ff9352df22d5c37f8379f9960a15c6
Wahram Andrikyan <wahram@andrikyan.de>
2020-12-25 02:08:05 +0100

    fix ICD parsing, added is_cad, improved cad readability and error handling

--- evaluate_variables_cad.py
+++ evaluate_variables_cad.py
@@ -4,21 +4,12 @@ import csv
 from atcs import *
 import sys
 from itertools import product
+from icd import is_cad
 
 true_positive = 0
 true_negative = 0
 false_positive = 0
 false_negative = 0
-
-CAD = ['I20.0', 'I20.00', 'I20.8', 'I20.9',
-       'I21.0', 'I21.1', 'I21.3', 'I21.4', 'I21.9',
-       'I23.6',
-       'I24.8', 'I24.9',
-       'I25.0', 'I25.11', 'I25.12', 'I25.13', 'I25.19', 'I25.29', 'I25.5', 'I25.9']
-
-print([n for n in CAD if 19 < int(n.split('.')[0][1:]) < 26])
-exit()
-
 
 nitrat = ranolazin | ismn | isdn | molsidomin | pentaerythrityltetranitrat
 p2y12_inhibitor = clopidogrel | prasugrel | ticagrelor
@@ -37,78 +28,95 @@ for row in reader:
 
 
 
-for ranolazin_score in range(0, 101, 50):
-    for nitrat_score in range(0, 101, 50):
-        for trapidil_score in range(0, 101, 50):
-            for ass_score in range(0, 101, 50):
-                for p2y12_inhibitor_score in range(0, 101, 50):
-                    for betablocker_score in range(0, 101, 50):
-                        for calciumkanalblocker_score in range(0, 101, 50):
-                            for statin_score in range(0, 101, 50):
-                                for ezetimib_score in range(0, 101, 50):
-                                    for row in data:
-                                        score = 0
+params = product(
+    [i for i in range(0, 101, 50)], # ranolazin
+    [i for i in range(0, 101, 50)], # nitrat
+    [i for i in range(0, 101, 50)], # trapidil
+    [i for i in range(0, 101, 50)], # ass
+    [i for i in range(0, 101, 50)], # p2y12_inhibitor
+    [i for i in range(0, 101, 50)], # betablocker
+    [i for i in range(0, 101, 50)], # calciumkanalblocker
+    [i for i in range(0, 101, 50)], # statin
+    [i for i in range(0, 101, 50)]  # ezetimib
+)
 
+for param in params:
+    ranolazin_score, nitrat_score, trapidil_score, ass_score, p2y12_inhibitor_score, betablocker_score, \
+    calciumkanalblocker_score, statin_score, ezetimib_score = param
 
-                                        atc_codes = set()
-                                        for pos in range(1, 9 + 1):
-                                            row_name = 'atc_0' + str(pos)
-                                            if row[row_name]:
-                                                atc_codes.add(row[row_name])
-                                        for pos in range(10, 20 + 1):
-                                            row_name = 'atc_' + str(pos)
-                                            if row[row_name]:
-                                                atc_codes.add(row[row_name])
+    for row in data:
+        score = 0
 
-                                        if ranolazin & atc_codes:
-                                            score += ranolazin_score
+        atc_codes = set()
+        for pos in range(1, 25 + 1):
+            row_name = 'atc_%02d' % pos
+            if row[row_name]:
+                atc_codes.add(row[row_name])
 
-                                        if nitrat & atc_codes:
-                                            score += nitrat_score
+        icd_codes = set()
+        for pos in range(1, 20 + 1):
+            row_name = 'icd10_%02d' % pos
+            if row[row_name]:
+                icd_codes.add(row[row_name])
 
-                                        if trapidil & atc_codes:
-                                            score += trapidil_score
+        if ranolazin & atc_codes:
+            score += ranolazin_score
 
-                                        if ass & atc_codes:
-                                            score += ass_score
+        if nitrat & atc_codes:
+            score += nitrat_score
 
-                                        if p2y12_inhibitor & atc_codes:
-                                            score += p2y12_inhibitor_score
+        if trapidil & atc_codes:
+            score += trapidil_score
 
-                                        if betablocker & atc_codes:
-                                            score += betablocker_score
+        if ass & atc_codes:
+            score += ass_score
 
-                                        if calciumkanalblocker & atc_codes:
-                                            score += calciumkanalblocker_score
+        if p2y12_inhibitor & atc_codes:
+            score += p2y12_inhibitor_score
 
-                                        if statin & atc_codes:
-                                            score += statin_score
+        if betablocker & atc_codes:
+            score += betablocker_score
 
-                                        if ezetimib & atc_codes:
-                                            score += ezetimib_score
+        if calciumkanalblocker & atc_codes:
+            score += calciumkanalblocker_score
 
-                                        if score >= 100 and any(item in CAD for item in row.values()):
-                                            true_positive += 1
-                                        if score >= 100 and not any(item in CAD for item in row.values()):
-                                            false_positive += 1
-                                        if score < 100 and not any(item in CAD for item in row.values()):
-                                            true_negative += 1
-                                        if score < 100 and any(item in CAD for item in row.values()):
-                                            false_negative += 1
+        if statin & atc_codes:
+            score += statin_score
 
-                                    original_stdout = sys.stdout  # Save a reference to the original standard output
-                                    with open('CAD_result.txt', 'a') as f:
-                                        print('Specificity:', true_negative / (true_negative + false_positive),
-                                            'Sensitivity:', true_positive / (true_positive + false_negative+0.00001),
-                                            ranolazin_score, nitrat_score, trapidil_score, ass_score,
-                                            p2y12_inhibitor_score, betablocker_score, calciumkanalblocker_score,
-                                            statin_score, ezetimib_score, file=f) # avoid division by zero
+        if ezetimib & atc_codes:
+            score += ezetimib_score
 
-                                        print('True Positives:', true_positive, 'True Negatives:', true_negative,
-                                              'False Positives:', false_positive,
-                                              'False Negatives:',
-                                              false_negative)  # validation: CAD(true) - true_positive = false_negative
-                                        true_positive = 0
-                                        true_negative = 0
-                                        false_positive = 0
-                                        false_negative = 0
+        if score >= 100 and any([is_cad(icd) for icd in icd_codes]):
+            true_positive += 1
+        if score >= 100 and not any([is_cad(icd) for icd in icd_codes]):
+            false_positive += 1
+        if score < 100 and not any([is_cad(icd) for icd in icd_codes]):
+            true_negative += 1
+        if score < 100 and any([is_cad(icd) for icd in icd_codes]):
+            false_negative += 1
+
+    original_stdout = sys.stdout  # Save a reference to the original standard output
+    with open('CAD_result2.txt', 'a') as f:
+        try:
+            specificity = true_negative / (true_negative + false_positive)
+        except:
+            specificity = 1
+
+        try:
+            sensitivity = true_positive / (true_positive + false_negative)
+        except:
+            sensitivity = 1
+
+        print('Specificity:', specificity,
+              'Sensitivity:', sensitivity,
+              ranolazin_score, nitrat_score, trapidil_score, ass_score,
+              p2y12_inhibitor_score, betablocker_score, calciumkanalblocker_score,
+              statin_score, ezetimib_score, file=f)
+        print('True Positives:', true_positive, 'True Negatives:', true_negative,
+              'False Positives:', false_positive,
+              'False Negatives:',
+              false_negative)  # validation: CAD(true) - true_positive = false_negative
+        true_positive = 0
+        true_negative = 0
+        false_positive = 0
+        false_negative = 0
--- icd.py
+++ icd.py
@@ -2,6 +2,8 @@ from functools import cache
 
 @cache
 def is_chf(icd):
+    if not icd:
+        return False
     return icd.startswith('I11.0') \
            or icd.startswith('I13.0') \
            or icd.startswith('I50.0') \
@@ -9,6 +11,11 @@ def is_chf(icd):
            or icd == 'I50.9' \
            or icd == 'R57.0'
 
-# @cache
-# def is_cad und die anderen Krankheiten
-
+@cache
+def is_cad(icd):
+    if not icd:
+        return False
+    try:
+        return 19 < int(icd.split('.')[0][1:]) < 26
+    except:
+        return False
